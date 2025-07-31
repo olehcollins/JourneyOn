3a83e3de3a4d5a36fabc0b7ea0b84ec489737045
olehcollins <159178589+olehcollins@users.noreply.github.com>
2025-07-31 14:53:17 +0100

    increased token expiration date

--- JourneyOn_Server/Infrastructure/Services/AccessTokenService.cs
+++ JourneyOn_Server/Infrastructure/Services/AccessTokenService.cs
@@ -58,7 +58,7 @@ public sealed class AccessTokenService (IConfiguration configuration, UserManage
         {
             {"user_id", user.Id.ToString()},
             { "access_token", accessToken },
-            { "token_expiration", DateTime.UtcNow.AddMinutes(30).ToString("o")},
+            { "token_expiration", DateTime.UtcNow.AddMinutes(1440).ToString("o")},
         };
     }
 }
